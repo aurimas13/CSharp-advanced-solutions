81bad8cb30a43025e6d10b5b3b69f6daec15f965
aurimas13 <aurimas.nausedas@gmail.com>
2021-08-18 17:14:10 +0300

    Add files via upload

---
+++ InheritanceC/InheritanceC/ImagePost.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceC
+{
+    // ImagePost derives from Post and adds a property (ImageURL) and two constructors
+
+    class ImagePost :Post
+    {
+        public string ImageURL { get; set; }
+
+        public ImagePost() { }
+
+        public ImagePost(string title, string sendByUsername, string imageURL, bool isPublic)
+        {
+            // The following properties and the GetNexID method are inherited from Post.
+            this.ID = GetNextID();
+            this.Title = title;
+            this.SendByUsername = sendByUsername;
+            this.IsPublic = isPublic;
+
+            // Property ImageUrl is a member of ImagePost, but not of Post
+            this.ImageURL = imageURL;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} - {1} - {2} - by {3}", this.ID, this.Title, this.ImageURL, this.SendByUsername);
+        }
+
+    }
+}
---
+++ InheritanceC/InheritanceC/VideoPost.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InheritanceC
+{
+    class VideoPost:Post
+    {
+        // member fields
+        protected bool isPlaying = false;
+        protected int currDuration = 0;
+        Timer timer;
+
+        // Properties
+        protected string VideoURL { get; set; }
+        protected int Length { get; set; }
+
+        public VideoPost() { }
+
+        public VideoPost(string title, string sendByUsername, string videoURL, bool isPublic, int length)
+        {
+            // The following properties and the GetNexID method are inherited from Post.
+            this.ID = GetNextID();
+            this.Title = title;
+            this.SendByUsername = sendByUsername;
+            this.IsPublic = isPublic;
+
+            // Property VideoURL is a member of VideoPost, but not of Post
+            this.VideoURL = videoURL;
+            this.Length = length;
+
+        }
+
+        public void Play()
+        {
+            if (!isPlaying)
+            {
+                isPlaying = true;
+                Console.WriteLine("Playing");
+                timer = new Timer(TimerCallback, null, 0, 1000);
+            }
+            
+        }
+
+        private void TimerCallback(Object o)
+        {
+            if(currDuration < Length)
+            {
+                currDuration++;
+                Console.WriteLine("Video at {0}s", currDuration);
+                GC.Collect();
+            }
+            else
+            {
+                Stop();
+            }
+        }
+
+        public void Stop()
+        {
+            if (isPlaying)
+            {
+                isPlaying = false;
+                Console.WriteLine("Stopped at {0}s", currDuration);
+                currDuration = 0;
+                timer.Dispose();
+            }
+            
+        }
+
+
+        public override string ToString()
+        {
+            return String.Format("{0} - {1} - {2} - {3} - by {4}", 
+                this.ID, this.Title, this.VideoURL, this.Length, this.SendByUsername);
+        }
+
+
+    }
+}
