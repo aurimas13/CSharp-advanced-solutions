a352b2dc8f1d7ba9e8a094bd4ebe82c12b98059b
aurimas13 <aurimas.nausedas@gmail.com>
2021-08-18 17:00:31 +0300

    Add files via upload

--- ArrayListC/ArrayListC/Program.cs
+++ ArrayListC/ArrayListC/Program.cs
@@ -1,1 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ArrayListC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declaring an ArrayList with undefined amount of object
+            ArrayList myArrayList = new ArrayList();
+            // declaring an ArrayList with defined amount of object
+            ArrayList myArrayList2 = new ArrayList(100);
+
+            myArrayList.Add(25);
+            myArrayList.Add("Hello");
+            myArrayList.Add(13.37);
+            myArrayList.Add(13);
+            myArrayList.Add(128);
+            myArrayList.Add(25.3);
+            myArrayList.Add(13);
+
+            // delete element with specific value from the arraylist
+            myArrayList.Remove(13);
+            myArrayList.Remove(13);
+            myArrayList.Remove(13);
+            // delete element at specific position
+            myArrayList.RemoveAt(0);
+
+            Console.WriteLine(myArrayList.Count);
+
+            double sum = 0;
+
+            foreach (object obj in myArrayList)
+            {
+                if (obj is int)
+                {
+                    sum += Convert.ToDouble(obj);
+                }
+                else if (obj is double)
+                {
+                    sum += (double)obj;
+                }
+                else if (obj is string)
+                {
+                    Console.WriteLine(obj);
+                }
+            }
+
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
