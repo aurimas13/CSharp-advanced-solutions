3e28c32cb7ccd0116864e9097cc47649172df75c
Aurimas A. Nausėdas <aurimas.nausedas@gmail.com>
2021-11-08 09:19:05 +0200

    Update Program.cs

--- ArrayListC/ArrayListC/Program.cs
+++ ArrayListC/ArrayListC/Program.cs
@@ -1,5 +1,5 @@
-// Created by Aurimas A. Nausedas on 18/08/21.
-// Updated by Aurimas A. Nausedas on 08/11/21.
+// Created by Aurimas A. Nausedas on 08/18/21.
+// Updated by Aurimas A. Nausedas on 11/08/21.
 
 using System;
 using System.Collections;
